fc0e4e21c7b25cdca7bbc0c29ce42dd19f7825c2
Zhdanova Ekaterina <zhdanova.ekaterina.95@yandex.ru>
2023-10-31 21:30:03 +0300

    init commit

---
+++ core/config.py
@@ -0,0 +1,26 @@
+# Конфиг для веб-приложения
+
+
+import os
+import logging
+
+from dotenv import load_dotenv
+
+
+logger = logging.getLogger(__name__)
+load_dotenv()
+
+
+# Настройки базы данных
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
+
+
+# Настройки логгирования
+log_level = logging.INFO
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
+
---
+++ core/src/helpers.py
@@ -0,0 +1,48 @@
+# Общие функции для настройки приложения
+
+
+import logging
+from time import time
+
+from core import config
+
+logger = logging.getLogger(__name__)
+
+
+def timetracking(foo):
+    """
+    Декоратор для трекинга времени выполнения функции
+    :param foo:
+    :return:
+    """
+
+    async def track_time():
+        start_time = time()
+        logger.info('---Started working---')
+
+        await foo()
+
+        end_time = time()
+        total_time = round((end_time - start_time), 3)
+        logger.info(f'---Finished in {total_time} s.')
+
+    return track_time
+
+
+def set_preferences():
+    """
+    Задаем глобальные настройки модуля логгирования
+    :return:
+    """
+
+    handlers = []
+    console_handler = logging.StreamHandler()
+    handlers.append(console_handler)
+
+    logging.basicConfig(
+        force=True,
+        level=config.log_level,
+        format=config.LOG_FORMAT,
+        datefmt=config.LOG_DATEFMT,
+        handlers=handlers
+    )
